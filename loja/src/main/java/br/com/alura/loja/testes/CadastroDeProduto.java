8b4b08af875aea1f1e55e4959727019a170f9fdf
Jurandi Junior <jurandijr20@gmail.com>
2023-04-05 20:57:53 -0300

    Consultando entidades

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,36 +1,45 @@
 package br.com.alura.loja.testes;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
+import br.com.alura.loja.dao.CategoriaDao;
+import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 	
 	public static void main(String[] args) {
+		cadastrarProduto();
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao produtoDao = new ProdutoDao(em); 
+		 
+		 Produto p = produtoDao.buscaPorId(1l);
+		 System.out.println(p.getPreco());
+		 
+		 List<Produto> todos = produtoDao.buscarTodos();
+		 todos.forEach(p2 -> System.out.println(p.getNome()));
+	}
+
+	private static void cadastrarProduto() {
 		Categoria celulares = new Categoria("CELULARES");
-//		Produto celular = new Produto("Xiaomi Redmi","Muito legal!", new BigDecimal("900"), celulares);
+		Produto celular = new Produto("Xiaomi Redmi","Muito legal!", new BigDecimal("900"), celulares);
         
         EntityManager em = JPAUtil.getEntityManager();
-//      ProdutoDao produtoDao = new ProdutoDao(em); 
-//      CategoriaDao categoriaDao = new CategoriaDao(em); 
+        ProdutoDao produtoDao = new ProdutoDao(em); 
+        CategoriaDao categoriaDao = new CategoriaDao(em); 
         
         em.getTransaction().begin();
         
-//      categoriaDao.cadastrar(celulares);
-//      produtoDao.cadastrar(celular);
+	    categoriaDao.cadastrar(celulares);
+	    produtoDao.cadastrar(celular);
         
-        em.persist(celulares); // De transient para Managed
-        celulares.setNome("JURA");
-        
-        em.flush();
-        em.clear();
-        
-        celulares = em.merge(celulares); //De Detached para Managed
-        celulares.setNome("1234");  
-        em.flush();
-        em.remove(celulares);
-        em.flush();
+        em.getTransaction().commit();
+        em.close();
 	}
 
 }
