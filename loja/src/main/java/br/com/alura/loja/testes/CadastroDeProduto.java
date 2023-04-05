e8c7e74e4d7f8ea8702dcd2b3b193d08ec085327
Jurandi Junior <jurandijr20@gmail.com>
2023-04-05 20:25:12 -0300

    Estados no insert da entidade

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,36 +1,32 @@
 package br.com.alura.loja.testes;
 
-import java.math.BigDecimal;
-
 import javax.persistence.EntityManager;
 
-import br.com.alura.loja.dao.CategoriaDao;
-import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Categoria;
-import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 	
 	public static void main(String[] args) {
 		Categoria celulares = new Categoria("CELULARES");
-		
-		
-		Produto celular = new Produto("Xiaomi Redmi","Muito legal!", new BigDecimal("900"), celulares);
+//		Produto celular = new Produto("Xiaomi Redmi","Muito legal!", new BigDecimal("900"), celulares);
         
         EntityManager em = JPAUtil.getEntityManager();
-        ProdutoDao produtoDao = new ProdutoDao(em); 
-        CategoriaDao categoriaDao = new CategoriaDao(em); 
+//      ProdutoDao produtoDao = new ProdutoDao(em); 
+//      CategoriaDao categoriaDao = new CategoriaDao(em); 
         
         em.getTransaction().begin();
         
-        categoriaDao.cadastrar(celulares);
-        produtoDao.cadastrar(celular);
+//      categoriaDao.cadastrar(celulares);
+//      produtoDao.cadastrar(celular);
+        
+        em.persist(celulares); // De transient para Managed
+        celulares.setNome("JURA");
         
         em.getTransaction().commit();
-        em.close();
+        em.close(); // Fechando o EntityManager
         
-		
+        celulares.setNome("1234");  // Qualquer alteração após o close é ignorada. 
 	}
 
 }
