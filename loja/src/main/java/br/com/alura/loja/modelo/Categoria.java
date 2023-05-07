7f97244cb9f13c5e6d3631177895625fb837b8a8
Jurandi Junior <jurandijr20@gmail.com>
2023-05-06 22:25:04 -0300

    Mapeamento de chaves compostas - corrigida a annotation

--- loja/src/main/java/br/com/alura/loja/modelo/Categoria.java
+++ loja/src/main/java/br/com/alura/loja/modelo/Categoria.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.modelo;
 
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -7,6 +8,7 @@ import javax.persistence.Table;
 @Table(name = "categorias")
 public class Categoria {
 
+	@EmbeddedId
 	private CategoriaId id;
 	
 	public Categoria() {
--- loja/src/main/java/br/com/alura/loja/modelo/CategoriaId.java
+++ loja/src/main/java/br/com/alura/loja/modelo/CategoriaId.java
@@ -1,10 +1,17 @@
 package br.com.alura.loja.modelo;
 
+import java.io.Serializable;
+
 import javax.persistence.Embeddable;
 
 @Embeddable
-public class CategoriaId {
+public class CategoriaId implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	private String nome;
 	private String tipo;
 
